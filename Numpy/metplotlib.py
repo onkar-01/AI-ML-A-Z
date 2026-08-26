a0b24c898b3156d0c9681b9b4f71a4a898fedc4d
Onkar Vatsa <onkarvatsa@gmail.com>
2026-08-26 14:23:04 +0530

    seaborn-and-matplotlib

--- Numpy/metplotlib.py
+++ Numpy/metplotlib.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import seaborn as sns
 
-expences = pd.DataFrame([
+expenses = pd.DataFrame([
     {"category": "food",     "amount": 250,   "city": "Delhi"},
     {"category": "travel",   "amount": 1200,  "city": "Mumbai"},
     {"category": "shopping", "amount": 500,   "city": "Delhi"},
@@ -16,8 +16,24 @@ expences = pd.DataFrame([
 
 # matplotlib raw histogram
 fig, ax = plt.subplot(figsize=(6,4))
-ax.hist(expences["amount"], bins=6, color="#ff0000", edgecolor="#fff")
+ax.hist(expenses["amount"], bins=6, color="#ff0000", edgecolor="#fff")
 ax.set_xlabel("Amount (₹)")
 ax.set_ylabel("Count")
 ax.set_title("Distribution of Expense Amounts")
 fig.savefig("hist_matplotlib.png")
+
+# seaborn histogram
+total = expenses.groupby("category")["amount"].sum().reset_index()
+fig2, ax2 = plt.subplot(figsize=(6,4))
+sns.barplot(data=total,x="category",y="amount",hue="category",ax=ax2,legend=False)
+ax2.set_title("Total spend by Category")
+fig2.savefig("bar_seaborn.png")
+plt.show()
+
+
+total2 = expenses.groupby("city")["amount"].sum().reset_index()
+fig3, ax3 = plt.subplots(figsize=(6,4))
+sns.barplot(data=total2,x="city",y="amount",hue="city",ax=ax3,legend=False)
+ax3.set_title("Total spend by city")
+fig3.savefig("bar_city_seaborn.png")
+plt.show()
